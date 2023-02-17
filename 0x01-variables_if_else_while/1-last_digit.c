18271e809a792fe525a338d804ddb0711686b17e
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 <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>
2023-02-17 15:41:49 -0800

    print the last digit of a number and then apply a conditional statment

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,17 +8,18 @@
 int main(void)
 {
 	int n;
-	int l = n % 10;
+	int l;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	l = n % 10;
 	if (l > 5)
 	{
-		printf("Last digit of %d is %d and greather than 5 \n", (n, l));
+		printf("Last digit of %d is %d and greather than 5 \n", n, l);
 	}
 	else if (l < 6)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0 \n", (n, l));
+		printf("Last digit of %d is %d and is less than 6 and not 0 \n", n, l);
 	}
 	else
 	{
