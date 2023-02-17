1f7ae62db85754583987a4c885763e2c5f7e314c
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 <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>
2023-02-17 06:29:31 -0800

    use printf statment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,8 @@
  */
 #include<stdio.h>
 int main(void)
-       /* this progarm prints a string using puts fuction*/	
+/* this progarm prints a string using puts fuction*/
 {
 	puts("Programming is like building a multilingual puzzle ");
-	
-	/* return zero*/
-	return(0);
+	return(0);/* return zero*/
 }
