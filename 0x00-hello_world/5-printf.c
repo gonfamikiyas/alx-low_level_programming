7a18405255b8f15e1a15eaf9d7f607ab961b968d
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 <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>
2023-02-17 07:08:08 -0800

    print using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
-*main - Entry point
-*Description: 'this program print a string
-*Return: returns 0
-*/
+ *main - Entry point
+ *Description: 'this program print a string
+ *Return: returns 0
+ */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
 }
