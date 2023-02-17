61792068840686b12b8e5459adb4e7b1bbef09f4
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 <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>
2023-02-17 09:59:49 -0800

    prints the size of different data type in C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Discription: this code prints the size of different data types in C
+ * Return - returns 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int l;
+	long long int L;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of an long int %lu byte(s)\n", sizeof(l));
+	printf("Size of an long long int: %lu byte(s)\n", sizeof(L));
+	return (0);
+}
