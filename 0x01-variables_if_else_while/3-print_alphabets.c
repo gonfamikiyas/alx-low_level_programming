40c924b95cb29ed7cc84a86a6796d608aed82d9c
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 <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>
2023-02-17 16:11:05 -0800

    letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+*main - Entery point
+*Return: return 0 if run succesfully
+*/
+int main(void)
+{
+	char l = 'a';
+	char L = 'A';
+
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	while (L <= 'Z')
+	{
+		putchar(L);
+		L++;
+	}
+	putchar('\n');
+	return (0);
+}
