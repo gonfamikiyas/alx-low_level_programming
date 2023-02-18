8c6acbcd458cf69fcd8c8f36122de42f7151b9b3
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 <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>
2023-02-17 16:21:24 -0800

    print from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+*main - Entery point
+*Return: return 0 if run succesfully
+*/
+int main(void)
+{
+	int l;
+
+	for (l = 0; l < 10; l++)
+	{
+		printf("%d",l);
+	}
+	putchar('\n');
+	return (0);
+}
