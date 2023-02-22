ce1b3a4bb099a6856de6158a130eb2567f7d5c16
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 <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>
2023-02-21 16:55:22 -0800

    Write a function that prints 10 times the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+*print_alphabet_x10 - Entery point
+*Return: return 0 if run succesfully
+*/
+void print_alphabet_x10(void)
+{
+	char l = 'a';
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		l = 'a';
+		while (l <= 'z')
+		{
+			_putchar(l);
+			l++;
+		}
+	putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,6 @@
 #define STRING_UTILS_H
 
 int _putchar(char c);
-void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif /* STRING_UTILS_H */
