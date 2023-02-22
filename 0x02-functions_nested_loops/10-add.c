22a623057be86f5f9db17e02df40addb72f12141
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 <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>
2023-02-21 18:55:51 -0800

    final

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ *add - Entery point
+ *@s: string
+ *@f: int
+ *Return: return 0 if run succesfully
+ */
+int add(int s, int f)
+{
+	int h;
+
+	h = s + f;
+	return (h);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,5 @@
 #define STRING_UTILS_H
 
 int _putchar(char c);
-void jack_bauer(void);
+int add(int, int);
 #endif /* STRING_UTILS_H */
