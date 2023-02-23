528088ef2978d6ab7ad52cd25eb958db5827e92c
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 <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>
2023-02-23 11:16:53 -0800

    multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *mul - Entery point
+ *@c: int
+ *@d: int
+ *Return: return 0 if run succesfully
+ */
+int mul(int c, int d)
+{
+	return (c * d);
+}
