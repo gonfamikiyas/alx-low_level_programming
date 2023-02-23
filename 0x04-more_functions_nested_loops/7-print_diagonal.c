87694ad38e1df8025ed0cd5a7128bb5e0ee8c545
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 <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>
2023-02-23 13:18:06 -0800

    print backslash

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,19 +13,19 @@ void print_diagonal(int c)
 {
 	int d, f;
 
-	for (d = 0; d < c; d++)
+	if (c <= 0)
 	{
-		if (c <= 0)
-		{
-			continue;
-		} else
+		_putchar('\n');
+	} else
+	{
+		for (d = 0; d < c; d++)
 		{
 			for (f = 0; f < d; f++)
 			{
 				_putchar(32);
 			}
 		_putchar(92);
+		_putchar('\n');
 		}
-	_putchar('\n');
 	}
 }
