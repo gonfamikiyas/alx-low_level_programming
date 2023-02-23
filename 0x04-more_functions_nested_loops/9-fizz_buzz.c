2773f799a16e6f22a6d5ec81072ee277d75acfe9
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 <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>
2023-02-23 13:52:09 -0800

    Write a program that prints the numbers from 1 to 100, followed by a new line

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *main - Entery point
+ *@d int
+ *Return: return 0 if run succesfully
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 1; d <= 100; d++)
+	{
+		if (d % 3 == 0 && d % 5 == 0)
+		{
+			printf("FizzBuzz");
+		} else if (d % 3 == 0)
+		{
+			printf("Fizz");
+		} else if (d % 5 == 0)
+		{
+			printf("Buzz");
+		} else
+		{
+			printf("%d", d);
+		}
+		printf("\n");
+	}
+	return (0);
+}
