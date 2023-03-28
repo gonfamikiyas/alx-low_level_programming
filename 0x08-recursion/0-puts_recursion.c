79da8d23d69e26484f02bfdc26892b25bdd7c6bb
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 <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>
2023-03-28 12:49:33 -0700

    Write a function that prints a string, followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+*_puts_recursion - check the code
+*@s: char
+*Return: Always 0.
+*/
+void _puts_recursion(char *s)
+{
+	puts(s);
+	return;
+
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef STRING_UTILS_H
+#define STRING_UTILS_H
+
+int _putchar(char c);
+int _isupper(int c);
+char *_strcpy(char *dest, char *src);
+void _puts_recursion(char *s);
+#endif /* STRING_UTILS_H */
