bb71c0b02e44ab375150e5cf51cf228a262feaa3
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 <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>
2023-03-29 08:21:50 -0700

    a function that returns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+*_strlen_recursion - returns the len of a string
+*@s:char
+*@i - int
+*Return: Always 0.
+*/
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = strlen(s);
+	return (i);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,4 +6,5 @@ int _isupper(int c);
 char *_strcpy(char *dest, char *src);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 #endif /* STRING_UTILS_H */
