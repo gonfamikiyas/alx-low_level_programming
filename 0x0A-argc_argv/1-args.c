a974586f88ec7c1a2b9d249f0f563a600d9829d6
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 <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>
2023-03-31 00:01:18 -0700

    a program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - return value
+ *@argc: int
+ *@argv: char
+ *Return: value
+ */
+int main(int argc, char **argv)
+{
+	char i;
+
+	i = strlen(*argv);
+	if (argc != i)
+	{
+		printf("%d\n", argc - 1);
+	}
+	exit(EXIT_SUCCESS);
+}
