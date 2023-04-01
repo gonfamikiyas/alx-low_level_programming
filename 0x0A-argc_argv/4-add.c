4245bd257d49e55a7ca54048c3de79d66813d6ad
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 <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>
2023-03-31 21:35:09 -0700

    betty styple code correction

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@
 int main(int argc, char **argv)
 {
 	int i, d;
-	long unsigned int j;
+	unsigned long int j;
 
 	if (argc > 1)
 	{
@@ -34,10 +34,9 @@ int main(int argc, char **argv)
 		}
 		printf("%d \n", d);
 	}
-	else if(argc <= 1)
+	else if (argc <= 1)
 	{
 		printf("%d \n", 0);
 	}
 	return (0);
-	exit(EXIT_SUCCESS);
 }
