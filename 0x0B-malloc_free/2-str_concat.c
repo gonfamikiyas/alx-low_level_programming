f354884c63b31ed6a29331396263be7c170f1a20
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 <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>
2023-04-02 03:43:19 -0700

    a function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,57 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
+
 /**
-* str_concat - get ends of input and add together for size
-* @s1: input one to concat
-* @s2: input two to concat
-* Return: concat of s1 and s2
-*/
+ * str_concat - Concatenates two strings.
+ * @s1: The first string to concatenate.
+ * @s2: The second string to concatenate.
+ *
+ * Return: On success - Pointer to the concatenated string.
+ *         On failure - NULL.
+ */
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
-	int i, ci;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 = NULL)
+	if (s1 != NULL)
 	{
-		s1 = "";
+		while (s1[len1])
+			len1++;
 	}
-	if (s2 == NULL)
+	if (s2 != NULL)
 	{
-		s2 = "";
-		i = ci = 0;
+		while (s2[len2])
+			len2++;
 	}
-	while (s1[i] != '\0')
-	{
-		i++;
-	}
-	while (s2[ci] != '\0')
-	{
-		ci++;
-		conct = malloc(sizeof(char) * (i + ci + 1));
-	}
-	if (conct == NULL)
-	{
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
-	}
-	i = ci = 0;
-	while (s1[i] != '\0')
+
+	i = 0;
+
+	if (s1 != NULL)
 	{
-		conct[i] = s1[i];
-		i++;
+		while (s1[i])
+		{
+			s3[i] = s1[i];
+			i++;
+		}
 	}
-	while (s2[ci] != '\0')
+
+	j = 0;
+
+	if (s2 != NULL)
 	{
-		conct[i] = s2[ci];
-		i++, ci++;
+		while (s2[j])
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
 	}
-	conct[i] = '\0';
-	return (conct);
+
+	s3[i] = '\0';
+	return (s3);
 }
