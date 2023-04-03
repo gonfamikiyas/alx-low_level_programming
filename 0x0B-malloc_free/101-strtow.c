5251907376320c177f8ecffc09df63ca35e43c20
gonfamikiyas <gonfamikiyas@gmail.com>
2023-04-03 07:21:51 -0700

    correction on malloc_free tesk

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,11 +9,14 @@
  */
 int count_word(char *s)
 {
-	int flag = 0, c = 0, w = 0;
+	int flag, c, w;
 
-	for (; *s != '\0'; s++)
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
 	{
-		if (*s == ' ')
+		if (s[c] == ' ')
 			flag = 0;
 		else if (flag == 0)
 		{
@@ -21,6 +24,7 @@ int count_word(char *s)
 			w++;
 		}
 	}
+
 	return (w);
 }
 
@@ -34,22 +38,20 @@ int count_word(char *s)
 char **strtow(char *str)
 {
 	char **matrix, *tmp;
-	int i, k = 0, len = 0, words, c = 0, start = 0, end = 0;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	while (str[len])
-	{
+	while (*(str + len))
 		len++;
-	}
+
 	words = count_word(str);
+
 	if (words == 0)
-	{
 		return (NULL);
-	}
-	matrix = malloc(sizeof(char *) * (words + 1));
+
+	matrix = (char **)malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
-	{
 		return (NULL);
-	}
+
 	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
@@ -57,32 +59,22 @@ char **strtow(char *str)
 			if (c)
 			{
 				end = i;
-				tmp = malloc(sizeof(char) * (c + 1));
+				tmp = (char *)malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
-				{
-					for (i = 0; matrix[i]; i++)
-						free(matrix[i]);
-					free(matrix);
 					return (NULL);
-				}
 
 				while (start < end)
-				{
 					*tmp++ = str[start++];
-				}
 				*tmp = '\0';
-				matrix[k++] = tmp - c;
+				matrix[k] = tmp - c;
+				k++;
 				c = 0;
 			}
 		}
-		else
-		{
-			if (c++ == 0)
-			{
-				start = i;
-			}
-		}
+		else if (c++ == 0)
+			start = i;
 	}
+
 	matrix[k] = NULL;
 	return (matrix);
 }
