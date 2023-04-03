6faa01b0143d4e4dae8afc90b2a546bbbf362767
gonfamikiyas <gonfamikiyas@gmail.com>
2023-04-03 08:21:05 -0700

    a program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,48 +1,74 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-#include <stdlib.h>
 #include "main.h"
-/**
- * main - return value
- *@argc: int
- *@argv: char
- *@d - int
- *@c - int
- *@i - int
- *Return: value
- */
-int main(int argc, char **argv)
-{
-	int i, d;
-	unsigned long int j;
 
-	if (argc == 3)
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
 	{
-		d = 1;
-		for (i = 1; i <= argc - 1; i++)
-		{
-			for (j = 0; j <= strlen(argv[i]) - 1; j++)
-			{
-				if (atoi(*(argv + i)) < 0)
-				{
-					d = -1 * d;
-				}
-				else if (!(isdigit(argv[i][j])))
-				{
-					return (1);
-				}
-			}
-			d = d * atoi(*(argv + i));
-		}
-		printf("%d", d);
-		printf("\n");
+		len++;
 	}
-	else
+	while (i < len && f == 0)
 	{
-		printf("Error");
-		printf("\n");
+		if (s[i] == '-')
+		{
+			++d;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+			{
+				digit = -digit;
+			}
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				break;
+			}
+		f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
 		return (1);
 	}
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
 	return (0);
 }
